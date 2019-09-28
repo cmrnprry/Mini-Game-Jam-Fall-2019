50502d0c3ce8efd66a7a01e58006eb4440b626d8
Rebecca Malcolm <malcolm.r@northeastern.edu>
2019-09-28 15:40:11 -0400

    Imported assets, implemented Tiger and Rain

---
+++ Mini-Game-Jam fall 2019/Assets/Protecc/Scripts/Protecc_RainController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protecc_RainController : MonoBehaviour
+{
+    public ParticleSystem part;
+    public List<ParticleCollisionEvent> collisionEvents;
+
+    // Start is called before the first frame update, when the sprite is created.
+    void Start()
+    {
+        // Access components that were added to the sprite in Unity.
+        part = GetComponent<ParticleSystem>();
+        collisionEvents = new List<ParticleCollisionEvent>();
+    }
+
+    // OnParticleCollision is called when a particle in this system hits a collider.
+    private void OnParticleCollision(GameObject other)
+    {
+        int numCollisionEvents = part.GetCollisionEvents(other, collisionEvents);
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        Animator anim = other.GetComponent<Animator>();
+
+        if (numCollisionEvents > 0 && rb)
+        {
+            anim.SetBool("isHurt", true);
+        }
+    }
+}
---
+++ Mini-Game-Jam fall 2019/Assets/Protecc/Scripts/Protecc_TigerController.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Protecc_TigerController : MonoBehaviour
+{
+    public int score;
+    public float speed;
+    public bool isAccelerating;
+    public float accelerationForce;
+
+    private Rigidbody2D rb; // gives control of this sprite to the Unity physics engine
+
+    // Start is called before the first frame update, when the sprite is created.
+    void Start()
+    {
+        // Access components that were added to the sprite in Unity.
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // FixedUpdate is frame-rate independent, used for Unity physics calculations.
+    private void FixedUpdate()
+    {
+        if (isAccelerating)
+        {
+            // Add force to the sprite to move it forward...
+            rb.AddForce(Vector2.right * accelerationForce);
+        }
+        else
+        {
+            // Set the sprite's velocity to a constant speed...
+            rb.velocity = new Vector2(speed, rb.velocity.y);
+        }
+        score++;
+    }
+
+    // OnTriggerEnter is called once when a collider on this sprite enters a trigger collider.
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // If the Sprite collides with our Finish checkpoint...
+        if (collision.gameObject.CompareTag("Finish"))
+        {
+            // Sprite stops moving...
+            rb.velocity = Vector2.zero;
+
+            // Load the next mini game...
+        }
+
+    }
+}
