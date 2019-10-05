03037fad323e80c3db094d1d53974ebae0c54a25
Elijah Carey Cobb <cobb.e@northeastern.edu>
2019-10-05 15:06:53 -0400

    YES

---
+++ Mini-Game-Jam fall 2019/Assets/ElijahScript/Trampoline.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Trampoline : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    private Rigidbody rb;
+
+    public float speed = 10f;
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        float horiz = Input.GetAxis("Horizontal");
+        float vert = Input.GetAxis("Vertical");
+
+        rb.MovePosition(transform.position + new Vector3(speed * horiz,0,speed * vert) * Time.fixedDeltaTime * speed);
+        
+    }
+}
